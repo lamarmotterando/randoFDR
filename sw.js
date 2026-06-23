3e536411c3c8cd5ffb15fdbb362c4615c17052de
lamarmotterando <lamarmotterando@gmail.com>
2026-06-23 16:36:55 +0200

    Update sw.js
    (pas de clé)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache les ressources essentielles pour usage hors ligne
    ============================================================ */
 
-const CACHE_NAME = 'randofdr-v5';
+const CACHE_NAME = 'randofdr-v6';
 
 /* Fichiers à mettre en cache au démarrage */
 const CACHE_STATIC = [
